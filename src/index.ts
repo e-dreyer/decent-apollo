5c8e5c6719be39a0b8db7f2a4fd31058f26544ed
erik.dreyer <erik.dreyer11@gmail.com>
2022-06-29 15:10:42 +0200

    experimentation with prisma

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,88 @@ import { ApolloServer, gql } from "apollo-server-express";
 
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  const allProfiles = await prisma.profile.findMany();
+  const allBlogs = await prisma.blog.findMany();
+  const allBlogPosts = await prisma.blogPost.findMany();
+  const allBlogComments = await prisma.blogComment.findMany();
+  console.log(allUsers);
+  console.log(allProfiles);
+  //  console.log(allBlogs);
+  //  console.log(allBlogPosts);
+  //  console.log(allBlogComments);
+}
+
 // Main function to run the server
 async function startServer() {
   // Create an instance of express and an http server
   const app = express();
   const httpServer = createServer(app);
 
-  const prisma = new PrismaClient();
-
   // Define API schema
   const typeDefs = gql`
     type User {
       id: ID!
+      createdAt: String!
+      updatedAt: String!
+      blogs: [Blog]
+      blogPosts: [BlogPost]
+      blogComments: [BlogComment]
     }
 
     type Profile {
       id: ID!
+      createdAt: String!
+      updatedAt: String!
+      user: User
+      username: String
     }
 
     type Blog {
       id: ID!
+      createdAt: String!
+      updatedAt: String!
+      author: User
+      name: String!
+      description: String
+      blogPosts: [BlogPost]
+    }
+
+    type BlogPost {
+      id: ID!
+      createdAt: String!
+      updatedAt: String!
+      author: User
+      blog: Blog!
+      blogComments: [BlogComment]
+      title: String!
+      content: String!
+    }
+
+    type BlogComment {
+      id: ID!
+      createdAt: String!
+      updatedAt: String!
+      author: User
+      blogPost: BlogPost
+      parent: BlogComment
+      content: String
     }
 
     type Query {
-      users: [User]
-      profiles: [Profile]
-      blogs: [Blog]
+      users: [User]!
+      user: User!
+      profiles: [Profile]!
+      profile: Profile!
+      blogs: [Blog]!
+      blog: Blog!
+      blogPosts: [BlogPost]!
+      blogPost: BlogPost!
+      blogComments: [BlogComment]!
+      blogComment: BlogComment!
     }
   `;
 
@@ -37,13 +93,58 @@ async function startServer() {
   const resolvers = {
     Query: {
       users() {
-        prisma.user.findMany();
+        return prisma.user.findMany();
       },
+      user(id: number) {
+        return prisma.user.findUnique({
+          where: {
+            id: id,
+          },
+        });
+      },
+
       profiles() {
-        prisma.profile.findMany();
+        return prisma.profile.findMany();
       },
+      profile(id: number) {
+        return prisma.profile.findUnique({
+          where: {
+            id: id,
+          },
+        });
+      },
+
       blogs() {
-        prisma.blog.findMany();
+        return prisma.blog.findMany();
+      },
+      blog(id: number) {
+        return prisma.blog.findUnique({
+          where: {
+            id: id,
+          },
+        });
+      },
+
+      blogPosts() {
+        return prisma.blogPost.findMany();
+      },
+      blogPost(id: number) {
+        return prisma.blogPost.findUnique({
+          where: {
+            id: id,
+          },
+        });
+      },
+
+      blogComments() {
+        return prisma.blogComment.findMany();
+      },
+      blogComment(id: number) {
+        return prisma.blogComment.findUnique({
+          where: {
+            id: id,
+          },
+        });
       },
     },
   };
@@ -71,3 +172,4 @@ async function startServer() {
 
 // Start the server
 startServer();
+main();
